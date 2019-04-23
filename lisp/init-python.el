9e500571d1fe09671d5f3b7865c7330470c5347d
Herman Nurlygayanov <shkertik@gmail.com>
2019-04-23 18:28:28 +0500

    Python: * Disabled Anaconda cause it doesn't seem to be working. * Enabled Elpy instead.

--- lisp/init-python.el
+++ lisp/init-python.el
@@ -8,17 +8,26 @@
               auto-mode-alist))
 
 (require-package 'pip-requirements)
+(require-package 'elpy)
 
-(when (maybe-require-package 'anaconda-mode)
-  (after-load 'python
-    (add-hook 'python-mode-hook 'anaconda-mode)
-    (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
-  (after-load 'anaconda-mode
-    (define-key anaconda-mode-map (kbd "M-?") nil))
-  (when (maybe-require-package 'company-anaconda)
-    (after-load 'company
-      (after-load 'python
-        (push 'company-anaconda company-backends)))))
+(elpy-enable)
+(setq elpy-rpc-backend "jedi")
+;;(setenv "WORKON_HOME" "/home/qwe/code/python/sandbox/")
+;;(pyvenv-activate "/home/qwe/code/python/sandbox/venv/")
+
+;;; anaconda completion doesn't work for some reason
+;; (when (maybe-require-package 'anaconda-mode)
+;;   (after-load 'python
+;;     (add-hook 'python-mode-hook 'anaconda-mode)
+;;     (add-hook 'python-mode-hook 'anaconda-eldoc-mode))
+;;   (after-load 'anaconda-mode
+;;     (define-key anaconda-mode-map (kbd "M-?") nil))
+;;   (when (maybe-require-package 'company-anaconda)
+;;     (after-load 'company
+;;       (after-load 'python
+;;         (push 'company-anaconda company-backends)))))
+
+
 
 
 (provide 'init-python)
